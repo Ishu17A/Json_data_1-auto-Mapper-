5d6c367409e3636ff0880e9454ae677fc3710152
ishan <ishan@DESKTOP-TOATOCU>
2023-03-21 16:08:21 +0530

    Add project files.

---
+++ Json_data_1/Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json_data_1
+{
+    public class Class2
+    {
+
+    
+        public class Employee
+        {
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public string Address { get; set; }
+            public string Department { get; set; }
+        }
+    
+    }
+
+
+        public class EmployeeDTO
+        {
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public string Address { get; set; }
+            public string Department { get; set; }
+        }
+    
+
+
+}
---
+++ Json_data_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Newtonsoft.Json;
+using static Json_data_1.Class2;
+
+namespace Json_data_1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Employee employee = new Employee { Name = "ishan",
+                                Salary = 20000,
+                                Address = "london",
+                                Department = "it" };
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Employee, EmployeeDTO>());                
+            var mapper = new Mapper(config);
+            var x = mapper.Map<Employee, EmployeeDTO>(employee);
+            Console.WriteLine("Name: " + x.Name + ", Salary: " + x.Salary + ", Address: " + x.Address + ", Department: " + x.Department);
+            Console.ReadLine();
+
+        }
+    }
+}
